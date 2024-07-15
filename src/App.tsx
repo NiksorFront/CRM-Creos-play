15d3bdc35461649d5a481d44108b5b87d49d07d4
Niksor <Salfordyoutube@ya.ru>
2024-07-15 15:12:58 +0300

    Базовая React + TypeScript сборка

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
